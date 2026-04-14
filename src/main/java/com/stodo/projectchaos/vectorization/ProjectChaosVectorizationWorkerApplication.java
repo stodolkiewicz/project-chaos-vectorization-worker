1d536d97868977569561c930fcd95f9f087a6010
stodolkiewicz <stodoo@gmail.com>
2026-04-15 01:05:56 +0200

    pom.xml, properties files

---
+++ src/main/java/com/stodo/projectchaos/vectorization/ProjectChaosVectorizationWorkerApplication.java
@@ -0,0 +1,13 @@
+package com.stodo.projectchaos.vectorization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectChaosVectorizationWorkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectChaosVectorizationWorkerApplication.class, args);
+	}
+
+}
