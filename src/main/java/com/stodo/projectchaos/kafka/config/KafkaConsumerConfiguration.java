142cb6587e7958fd30f18ffec34b0aa2b48684e2
stodolkiewicz <stodoo@gmail.com>
2026-04-20 01:43:11 +0200

    wip10

--- src/main/java/com/stodo/projectchaos/kafka/config/KafkaConsumerConfiguration.java
+++ src/main/java/com/stodo/projectchaos/kafka/config/KafkaConsumerConfiguration.java
@@ -1,12 +1,12 @@
 package com.stodo.projectchaos.kafka.config;
 
 import io.confluent.kafka.serializers.KafkaAvroDeserializer;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.ByteArraySerializer;
 import org.apache.kafka.common.serialization.VoidDeserializer;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
@@ -18,32 +18,18 @@ import org.springframework.kafka.listener.DefaultErrorHandler;
 import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
 import org.springframework.util.backoff.FixedBackOff;
 
-import java.util.HashMap;
 import java.util.Map;
 
 @Slf4j
+@RequiredArgsConstructor
 @Configuration
 public class KafkaConsumerConfiguration {
 
-    @Value("${spring.kafka.consumer.bootstrap-servers}")
-    private String bootstrapServers;
-
-    @Value("${spring.kafka.consumer.group-id}")
-    private String consumerGroupId;
-
-    @Value("${spring.kafka.properties.schema.registry.url}")
-    private String schemaRegistryUrl;
-
-    @Value("${spring.kafka.consumer.properties.specific.avro.reader}")
-    private boolean ifSpecificAvroReader;
+    private final KafkaProperties kafkaProperties;
 
     @Bean
     public ConsumerFactory<Void, Object> consumerFactory() {
-        HashMap<String, Object> config = new HashMap<>();
-        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-        config.put(ConsumerConfig.GROUP_ID_CONFIG, consumerGroupId);
-        config.put("schema.registry.url", schemaRegistryUrl);
-        config.put("specific.avro.reader", ifSpecificAvroReader);
+        Map<String, Object> config = kafkaProperties.buildConsumerProperties(null);
 
         ErrorHandlingDeserializer<Void> keyDeserializer = new ErrorHandlingDeserializer<>(new VoidDeserializer());
         ErrorHandlingDeserializer<Object> valueDeserializer = new ErrorHandlingDeserializer<>(new KafkaAvroDeserializer());
@@ -58,11 +44,9 @@ public class KafkaConsumerConfiguration {
         ConcurrentKafkaListenerContainerFactory<Void, Object> factory =
                 new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(consumerFactory());
-        // creates 1 consumer thread
         factory.setConcurrency(1);
         factory.getContainerProperties().setPollTimeout(5000);
 
-        // will send the message to the same topic it got it from but ending with -dlt
         DeadLetterPublishingRecoverer recoverer = new DeadLetterPublishingRecoverer(kafkaTemplate);
 
         DefaultErrorHandler errorHandler = new DefaultErrorHandler(
@@ -80,8 +64,7 @@ public class KafkaConsumerConfiguration {
 
     @Bean
     ProducerFactory<Object, Object> producerFactory() {
-        Map<String, Object> config = new HashMap<>();
-        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        Map<String, Object> config = kafkaProperties.buildProducerProperties(null);
         config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, ByteArraySerializer.class);
         config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, ByteArraySerializer.class);
 
